e001f0439c35453c77e96e63ad4b647b5eeffa0a
Annie David <91792578+anniedavid13@users.noreply.github.com>
2023-05-26 23:49:18 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,15 +19,15 @@ export default function Document() {
 <!--         <meta name="title" content="Annie David –– Portfolio" />
         <meta
           name="description"
-          content="Portfolio website of front-end engineer Ken Nersisyan"
+          content="Portfolio website of front-end engineer Annie David"
         /> -->
 
         {/* <!-- Facebook Meta Tags --> */}
 <!--         <meta property="og:url" content="https://www.ken.engineer/" />
-        <meta property="og:title" content="Ken Nersisyan –– Portfolio" />
+        <meta property="og:title" content="Annie David –– Portfolio" />
         <meta
           property="og:description"
-          content="Portfolio website of front-end engineer Ken Nersisyan"
+          content="Portfolio website of front-end engineer Annie David"
         />
         <meta
           property="og:image"
@@ -38,10 +38,10 @@ export default function Document() {
 <!--         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="ken.engineer" />
         <meta property="twitter:url" content="https://www.ken.engineer/" />
-        <meta name="twitter:title" content="Ken Nersisyan –– Portfolio" />
+        <meta name="twitter:title" content="Annie David –– Portfolio" />
         <meta
           name="twitter:description"
-          content="Portfolio website of front-end engineer Ken Nersisyan"
+          content="Portfolio website of front-end engineer Annie David"
         />
         <meta
           name="twitter:image"
