d33fbd7b64fce7f313adcfcc807e22dfca99287b
Annie David <E20CSE068@bennett.edu.in>
2023-05-28 20:57:47 +0530

    updates

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,34 +20,26 @@ export default function Document() {
       <meta name="title" content="Annie David –– Portfolio" />
         <meta
           name="description"
-          content="Portfolio website of front-end engineer Ken Nersisyan"
+          content="Portfolio website of front-end engineer Annie David"
         />
 
         {/* <!-- Facebook Meta Tags --> */}
-      <meta property="og:url" content="https://www.ken.engineer/" />
-        <meta property="og:title" content="Ken Nersisyan –– Portfolio" />
+        <meta property="og:title" content="Annie David –– Portfolio" />
         <meta
           property="og:description"
           content="Portfolio website of front-end engineer Ken Nersisyan"
         />
-        <meta
-          property="og:image"
-          content="https://raw.githubusercontent.com/kens-visuals/ken.engineer/main/public/assets/seo-img.png"
-        />
+
 
         {/* <!-- Twitter Meta Tags --> */}
   <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="ken.engineer" />
-        <meta property="twitter:url" content="https://www.ken.engineer/" />
-        <meta name="twitter:title" content="Ken Nersisyan –– Portfolio" />
+        <meta property="twitter:domain" content="Annie David" />
+        <meta name="twitter:title" content="Annie David –– Portfolio" />
         <meta
           name="twitter:description"
-          content="Portfolio website of front-end engineer Ken Nersisyan"
+          content="Portfolio website of front-end engineer Annie David"
         />
-        <meta
-          name="twitter:image"
-          content="https://raw.githubusercontent.com/kens-visuals/ken.engineer/main/public/assets/seo-img.png"
-        /> 
+
 
         {/* Favicon */}
         <link
